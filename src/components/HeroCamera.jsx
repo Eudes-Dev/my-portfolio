42319f5c171846f2f3ec0ca928effa956f721181
Jek <jek@localhost.localdomain>
2025-04-24 00:03:09 +0000

    correction des bugs

--- src/components/HeroCamera.jsx
+++ src/components/HeroCamera.jsx
@@ -18,24 +18,3 @@ const HeroCamera = ({ children, isMobile }) => {
     )
 }
 export default HeroCamera
-
-import {useRef} from "react";
-import {useFrame} from '@react-three/fiber'
-import { easing } from 'maath'
-
-const HeroCamera = ({ children, isMobile }) => {
-    const groupRef = useRef()
-    useFrame((state, delta) => {
-        easing.damp3(state.camera.position, [0, 0, 20], 0.25, delta)
-
-        if (!isMobile) {
-            easing.dampE(groupRef.current.rotation, [-state.pointer.y / 3, state.pointer.x / 2, 0], 0.25, delta)
-        }
-    })
-    return (
-        <group ref={groupRef} scale={isMobile ? 1.2 : 1.25}>
-            {children}
-        </group>
-    )
-}
-export default HeroCamera
